75816d4634a82cb0d9fa8136c1024d7fc72461b3
Lawand <Lawand15.kong@gmail.com>
2023-06-08 12:01:22 +0100

    ;asfk.

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import './App.css';
+import Contact from '../Contact/Contact.js'
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className='application-header'>
+        <button>
+            <img alt="Logo"/>
+        </button>
+        <nav>
+          <button>Home</button>
+          <button>Offers</button>
+          <button>Mens</button>
+          <button>Womens</button>
+          <button>Kids</button>
+          <button>Costume design</button>
+          <button>About</button>
+          <button>Contact</button>
+
+        </nav>
+        <input placeholder='Search...'/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
+
+
