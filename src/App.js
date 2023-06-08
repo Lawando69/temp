b84b8dcd26a36a9de1551d50f7671306081f4d13
Lawand <Lawand15.kong@gmail.com>
2023-06-08 12:29:47 +0100

    Changed some things in app.js

--- src/App.js
+++ src/App.js
@@ -1,34 +1,58 @@
 import './App.css';
-import Contact from '../Contact/Contact.js'
-
+import Home from './Pages/Home.js'
+import About from './Pages/About.js'
+import Contact from './Pages/Contact.js'
+import Women from './Pages/Women.js'
+import Kids from './Pages/Kids.js'
+import Men from './Pages/Men.js'
+import CostumeDesign from './Pages/Costume-design.js' 
 
 function App() {
-  return (
-    <div className="App">
-      <header className='application-header'>
-        <button>
-            <img alt="Logo"/>
-        </button>
-        <div>
-          <nav>
-            <button>Home</button>
-            <button>Offers</button>
-            <button>Mens</button>
-            <button>Womens</button>
-            <button>Kids</button>
-            <button>Costume design</button>
-            <button>About</button>
-            <button>Contact</button>
+  const { isAdmin } = useSelector((state) => state.user);
 
-          </nav>
-        </div>
-        <input placeholder='Search...'/>
-      </header>
-    </div>
+  return (
+    <Router>
+      {
+        <Switch>
+          {isAdmin ? (
+            <>
+              <Topbar />
+              <div className="container">
+                
+                <Route exact path="/">
+                  <Home />
+                </Route>
+                <Route path="/About">
+                  <About />
+                </Route>
+                <Route path="/Contact">
+                  <Contact />
+                </Route>
+                <Route path="/Women">
+                  <Women />
+                </Route>
+                <Route path="/Kids">
+                  <Kids />
+                </Route>
+                <Route path="/Kids">
+                  <Kids />
+                </Route>
+                <Route path="/newproduct">
+                  <NewProduct />
+                </Route>
+              </div>
+            </>
+          ) : (
+            <Redirect to="/login" />
+          )}
+        </Switch>
+      }
+    </Router>
   );
 }
 
 export default App;
+
 
 
 
