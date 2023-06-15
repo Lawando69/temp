a9d8816ac9f7776b2fd89f0c98771a500dbae954
Lawando69 <Lawand15.kong@gmial.com>
2023-06-15 23:40:32 +0100

    Commit 8

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,16 +1,34 @@
-import React from "react";
-import { NavLink } from "react-router-dom"
-import logo from "../Assets/logo.png"
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+import logo from "../Assets/logo.png";
+import ReorderIcon from '@mui/icons-material/Reorder';
 import '../Styles/NavBar.css'
 
+
 function NavBar(){
+    const [openLinks, setOpenLinks] = useState(false);
+
+    const reOrderButton = () => {
+        setOpenLinks(!openLinks)
+    }
+
     return(
         
-        <div className="Nav-Bar">
-            <div className="Left-Side">
+        <div className="Navbar">
+            <div className="leftSide" id={openLinks ? "open" : "close"}>
                 <img src={logo} />
+                <div className="hiddenLinks">
+                    <NavLink to="/"> Home </NavLink>
+                    <NavLink to="Offers"> Offers </NavLink>
+                    <NavLink to="Men"> Men </NavLink>
+                    <NavLink to="Women"> Women </NavLink>
+                    <NavLink to="Kids"> Kids </NavLink>
+                    <NavLink to="Custom-design"> Custom design</NavLink>
+                    <NavLink to="About"> About </NavLink>
+                    <NavLink to="Contact"> Contact </NavLink>
+                </div>
             </div>
-            <div className="Right-Side">
+            <div className="rightSide">
                 <NavLink to="/"> Home </NavLink>
                 <NavLink to="Offers"> Offers </NavLink>
                 <NavLink to="Men"> Men </NavLink>
@@ -19,6 +37,9 @@ function NavBar(){
                 <NavLink to="Custom-design"> Custom design</NavLink>
                 <NavLink to="About"> About </NavLink>
                 <NavLink to="Contact"> Contact </NavLink>
+                <button onClick={reOrderButton}>
+                    <ReorderIcon />
+                </button>
             </div>
         </div>
     )
