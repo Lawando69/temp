a1194c4c01a3080281c4b1e57f9b4e9f50930d18
Lawando69 <Lawand15.kong@gmial.com>
2023-06-20 22:52:59 +0100

    Commit 10

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,11 +1,15 @@
 import React from "react";
-import { Instagram } from "material-ui/icons/Instagram";
+import InstagramIcon from '@mui/icons-material/Instagram';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import '../Styles/Footer.css'
 
 function Footer(){
     return(
         <div className="Footer">
             <div className="SocialMedia">
-                <Instagram />
+                <InstagramIcon /> <TwitterIcon /> <FacebookIcon /> <LinkedInIcon />
                 <a> &copy; 2023 T-shirts.com </a>
             </div>
         </div>
--- src/Pages/Home-page.js
+++ src/Pages/Home-page.js
@@ -2,6 +2,9 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import banner from '../Assets/Home-banner.png'
 import '../CSS/Home-page.css'
+import { HomeList } from '../ProductList/HomeList.js'
+import HomeItem from '../Components/HomeItem.js'
+import '../CSS/Home-page.css'
 
 function Home(){
 
@@ -12,9 +15,18 @@ function Home(){
     }
     return(
         <div className="Home">
-            <img className="Home-banner" src={banner} onClick={Sport} alt="Banner" />
-            <div className="HeaderContainer">
-                <h1> </h1>
+            <img src={banner} onClick={Sport} alt="Banner" />
+            <div className="HomeList">
+               {HomeList.map((menuItem, key) => {
+                    return (
+                        <HomeItem
+                            key={key}
+                            image={menuItem.image} 
+                            name={menuItem.name} 
+                            price={menuItem.price} 
+                        />
+                    );
+               })} 
             </div>
         </div>
     )
