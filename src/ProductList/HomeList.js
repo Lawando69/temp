cdcb97311ee5b97d089fee603aeede7ad4bf0114
Lawando69 <Lawand15.kong@gmail.com>
2023-07-10 16:38:48 +0100

    Improved the website

--- src/ProductList/HomeList.js
+++ src/ProductList/HomeList.js
@@ -1,6 +1,7 @@
 import Product01 from '../Assets/HomeList1.png';
 import Product02 from '../Assets/HomeList2.png';
 import Product03 from '../Assets/HomeList3.png';
+import Product04 from '../Assets/HomeList4.png';
 
 export const HomeList = [
     {
@@ -17,5 +18,10 @@ export const HomeList = [
         name: "GRAPHIC T-SHIRT",
         image: Product03,
         price: 29.99,
+    },
+    {
+        name: "ADICOLOR 70S VINTAGE CALI T-SHIRT",
+        image: Product04,
+        price: 37.68
     }
 ]
